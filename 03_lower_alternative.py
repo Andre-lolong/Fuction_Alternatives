8b9b53e0777ac764d705623e1c0121fc9bd71f7a
Andre <andrepetilunalolong@gmail.com>
2025-03-28 00:11:41 +0800

    created a function

--- 03_lower_alternative.py
+++ 03_lower_alternative.py
@@ -2,3 +2,12 @@
 # check for capital letters
 # get the ASCII number and shift uppercase to lowercase
 # add all the lowercase to the final result
+def lower_alternative(text):
+  result = ""
+  for letter in text:
+    if 'A' <= letter <= 'Z':
+      lowercase_letter = chr(ord(letter) + 32)
+      result += lowercase_letter
+    else:
+      result += letter
+  return result
