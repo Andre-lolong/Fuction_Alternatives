46abaabf31b47120b06010f75dd4cf14ff6a3a16
Andre <andrepetilunalolong@gmail.com>
2025-03-28 00:39:19 +0800

    created a function

--- 04_isupper_alternative.py
+++ 04_isupper_alternative.py
@@ -1,4 +1,8 @@
 # create a functiom
 # identify the letters
 # if lowercase then return false and true if uppercase
-
+def is_upper_manual(text):
+  for char in text:
+    if 'a' <= char <= 'z':
+      return False
+  return True
