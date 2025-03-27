817caeb6ce8e1702fcaecfc2a9c68892f0bc9ba2
Andre <andrepetilunalolong@gmail.com>
2025-03-28 01:06:21 +0800

    created a function

--- 05_endswith_alternative.py
+++ 05_endswith_alternative.py
@@ -1,3 +1,14 @@
 # create a function
 # check if it ends with the given suffix
 # return true if yes and false if not
+def endswith_alternative(text, suffix):
+    text_len = len(text)
+    suffix_len = len(suffix)
+
+    if suffix_len > text_len:
+        return False
+
+    if text[text_len - suffix_len:] == suffix:
+        return True
+    else:
+        return False
