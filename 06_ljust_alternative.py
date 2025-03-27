305700d081ed2c59235d17285de487cd394a22f1
Andre <andrepetilunalolong@gmail.com>
2025-03-28 01:33:03 +0800

    pseudocode

--- 06_ljust_alternative.py
+++ 06_ljust_alternative.py
@@ -2,3 +2,11 @@
 # choose width to be justified
 # add it to the text
 # display
+def ljust_manual(text, width):
+    text_len = len(text)
+    if text_len >= width:
+        return text
+
+    spaces_to_add = width - text_len
+    return text + " " * spaces_to_add
+
