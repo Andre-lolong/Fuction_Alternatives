366d96490e591e81df55bf54d9b16c825146b6b9
Andre <andrepetilunalolong@gmail.com>
2025-03-28 01:47:15 +0800

    created a function

--- 07_center_alternative.py
+++ 07_center_alternative.py
@@ -2,3 +2,13 @@
 # get the text and sapces from both sides
 # add the spaces to both sides to make sure the text is in the center
 # display
+def center_alternative(text, width):
+    text_len = len(text)
+    if text_len >= width:
+        return text
+
+    space = width - text_len
+    left_space = space // 2
+    right_space = space - left_space
+
+    return " " * left_space + text + " " * right_space
