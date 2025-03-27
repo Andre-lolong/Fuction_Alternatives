e7ef3038788b0d714be7afad586e907acd08e72b
Andre <andrepetilunalolong@gmail.com>
2025-03-27 23:25:50 +0800

    lstrip() alternative

--- 01_lstrip_alternative.py
+++ 01_lstrip_alternative.py
@@ -1,9 +1,13 @@
-def remove_leading_spaces(text):
+# create a function
+# check if the beginning is a space 
+# remove the space
+# print the output
+def lstrip_alternative(text):
     i = 0
     while i < len(text) and text[i].isspace():
         i += 1
     return text[i:]
 
-my_string = input("Enter something: ")
-result = remove_leading_spaces(my_string)
-print(result)
+string = input("Enter something: ")
+lstripped = lstrip_alternative(string)
+print(lstripped)
