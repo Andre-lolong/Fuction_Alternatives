f001d586d9fa905a79e79f5e036b21d946cebc57
Andre <andrepetilunalolong@gmail.com>
2025-03-27 23:49:56 +0800

    removeprefix() alternative

--- 02_removeprefix_alternative.py
+++ 02_removeprefix_alternative.py
@@ -6,3 +6,7 @@ def remove_prefix_manual(text, prefix):
   if text.startswith(prefix):
     return text[len(prefix):]
   return text
+
+main = input("Enter the main text: ")
+prefix = input("Enter a prefix: ")
+print(remove_prefix_manual(main, prefix))
