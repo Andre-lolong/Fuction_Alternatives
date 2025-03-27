b5683ebd61d82157a9cd2f086bcad8022589ed50
Andre <andrepetilunalolong@gmail.com>
2025-03-27 23:41:40 +0800

    created a function

--- 02_removeprefix_alternative.py
+++ 02_removeprefix_alternative.py
@@ -2,3 +2,7 @@
 # identify the main text and prefix
 # if the prefix is in the sentence, remove the prefix
 # print the text with removed prefix
+def remove_prefix_manual(text, prefix):
+  if text.startswith(prefix):
+    return text[len(prefix):]
+  return text
