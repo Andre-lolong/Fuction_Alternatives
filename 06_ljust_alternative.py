b7811011bec16c18ac6fd63153f837cb870b685d
Andre <andrepetilunalolong@gmail.com>
2025-03-28 01:36:03 +0800

    ljust() alternative

--- 06_ljust_alternative.py
+++ 06_ljust_alternative.py
@@ -10,3 +10,6 @@ def ljust_manual(text, width):
     spaces_to_add = width - text_len
     return text + " " * spaces_to_add
 
+text = input("Enter a text: ")
+spaces = int(input("Enter how many space you want: "))
+print(ljust_manual(text, spaces))
